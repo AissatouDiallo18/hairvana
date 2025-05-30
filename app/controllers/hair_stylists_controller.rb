8a4b0157efe825a2c1923e7a77e56555f94331bb
Santiago Crasemann <crasemannsantiago@gmail.com>
2025-05-30 15:04:37 +0200

    map added and header linked and fixed

--- app/controllers/hair_stylists_controller.rb
+++ app/controllers/hair_stylists_controller.rb
@@ -1,9 +1,17 @@
 class HairStylistsController < ApplicationController
-  
+
   def index
     @hair_stylists = HairStylist.all
+
+    @markers = @hair_stylists.geocoded.map do |hair_stylist|
+      {
+        lat: hair_stylist.latitude,
+        lng: hair_stylist.longitude,
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
   end
-  
+
   def show
     @hairstylist = HairStylist.find(params[:id])
     @timeslots = @hairstylist.time_slots
